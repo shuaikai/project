f370ea56209b6d885e586325ea62ba5cc2516553
unknown <ASUS@ASUS-PC.(none)>
2016-10-31 22:10:39 +0800

    Initial commit

---
+++ app/src/main/java/com/lsk/project/MyApp.java
@@ -0,0 +1,14 @@
+package com.lsk.project;
+
+import android.app.Application;
+
+/**
+ * Created by ASUS on 2016/10/31.
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
