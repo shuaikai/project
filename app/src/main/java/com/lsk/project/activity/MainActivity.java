18f02e55b5b07a53eb7f3319c6c29ca796cc2364
jhonwest <314413856@qq.com>
2016-11-01 11:13:43 +0800

    蒋维涛，部分初始界面

--- app/src/main/java/com/lsk/project/activity/MainActivity.java
+++ app/src/main/java/com/lsk/project/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package com.lsk.project.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.lsk.project.R;
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        System.out.println("克隆模板");
+        Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/lsk/project/activity/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.lsk.project.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by lenovo on 2016/11/1.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
